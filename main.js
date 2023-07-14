d2664b899f24ee154f1692e3217af456c24b910a
Rahil Ghanchi <rahiljakir786@gmail.com>
2023-07-15 01:26:19 +0530

    better way to hide my creds

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, setDoc, onSnapshot, addDoc, getDoc, updateDoc, doc } from "firebase/firestore";
+import firebaseConfig from './secret.json';
 
-const firebaseConfig = {
-
-}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
